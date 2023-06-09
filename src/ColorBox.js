b6ee479af0aa20d93585479494ef864488b14a6a
Sayed Shehata <sayedshehata33@gmail.com>
2023-06-09 18:41:41 +0300

    feat: :sparkles: Create ColorBox component

---
+++ src/ColorBox.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./ColorBox.css";
+
+class ColorBox extends Component {
+  render() {
+    return (
+      <div
+        className="ColorBox"
+        style={{ backgroundColor: this.props.background }}
+      >
+        <span>{this.props.name}</span>
+      </div>
+    );
+  }
+}
+
+export default ColorBox;
