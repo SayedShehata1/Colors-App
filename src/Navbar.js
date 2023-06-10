4afee1d1a6c8f8ecf1b18526f9c7c4fb0fc5db51
Sayed Shehata <sayedshehata33@gmail.com>
2023-06-10 19:15:21 +0300

    feat: :sparkles: Add select and material UI

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,10 +2,22 @@ import React, { Component } from "react";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { format: "hex" };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({ format: e.target.value });
+    this.props.handleChange(e.target.value);
+  }
   render() {
     const { level, changeLevel } = this.props;
+    const { format } = this.state;
     return (
       <header className="Navbar">
         <div className="logo">
@@ -22,6 +34,13 @@ class Navbar extends Component {
               onAfterChange={changeLevel}
             />
           </div>
+        </div>
+        <div className="select-container">
+          <Select value={format} onChange={this.handleChange}>
+            <MenuItem value="hex">Hex- #ffffff</MenuItem>
+            <MenuItem value="rgb">rgb- rgb(250,250,250)</MenuItem>
+            <MenuItem value="rgba">rgba- rgba(250,250,250,1.0)</MenuItem>
+          </Select>
         </div>
       </header>
     );
