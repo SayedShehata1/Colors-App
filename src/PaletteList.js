a1a602378e2030ddfa88bbdb83b09880f80e47b0
Sayed Shehata <sayedshehata33@gmail.com>
2023-06-12 17:28:50 +0300

    feat: :sparkles: Create newPaletteForm and add route for it

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import MiniPalette from "./MiniPalette";
 import { withStyles } from "@material-ui/styles";
 import styles from "./styles/PaletteListStyles";
+import { Link } from "react-router-dom";
 
 class PaletteList extends Component {
   goToPalette(id) {
@@ -14,10 +15,12 @@ class PaletteList extends Component {
         <div className={classes.container}>
           <nav className={classes.nav}>
             <h1>React Colors</h1>
+            <Link to="/palette/new">Create Palette</Link>
           </nav>
           <div className={classes.palettes}>
             {palettes.map((palette) => (
               <MiniPalette
+                key={palette.id}
                 {...palette}
                 handleClick={() => this.goToPalette(palette.id)}
               />
